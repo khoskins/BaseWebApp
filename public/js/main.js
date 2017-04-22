b3c48e708accbc735693c7d092ca591eb4945c00
khoskins <katiehoskins18@gmail.com>
2017-04-22 11:58:18 -0400

    changed image

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,6 @@ function myFunction() {
   $("#demo").html("Hello hello hello wazzup");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="/images/grumpy-cat.jpeg" />`);
+  $("#doge-image").append(`<img class="img-circle" src="/images/grumpy-cat.jpg" />`);
 }
 
